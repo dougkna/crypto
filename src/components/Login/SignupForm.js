f144b8e21c1a3efe2f8004c874958bc207e5e3d3
dougkna <dougkna@gmail.com>
2018-01-18 21:41:36 -0800

    minor fix

--- src/components/Login/SignupForm.js
+++ src/components/Login/SignupForm.js
@@ -64,7 +64,7 @@ export default class SignupForm extends Component {
           placeholder='Email'
           keyboardType='email-address'
           placeholderTextColor='#34495e'
-          returnKeyType='go'
+          returnKeyType='next'
           autoCapitalize='none'
           autoCorrect={false}
           onSubmitEditing={() => this.usernameInput.focus()}
